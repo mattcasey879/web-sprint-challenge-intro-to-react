8ff4cfc92710bce8411f89f2b50137d3606bd2e4
Matt <matthew-casey@lambdastudents.com>
2021-05-28 21:36:25 -0500

    finished MVP

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Character from './components/Character';
+import axios from 'axios';
+import styled from 'styled-components'
 
+const Outline = styled.h1`
+text-shadow: -1px -1px 0 #FFE81F, 1px -1px 0 #FFE81F, -1px 1px 0 #FFE81F, 1px 1px 0 #FFE81F;
+`
 const App = () => {
+const [characters, setCharacters] = useState([]);
+
+useEffect(() => {
+  axios
+  .get('https://swapi.dev/api/people')
+  .then(res => {
+    setCharacters(res.data)
+  })
+  .catch(err => {console.log(err)})
+}, [])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+console.log(characters);
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <Outline> Star Wars Characters </Outline>
+      {
+        characters.map(char => {
+          return (
+            <Character key={char.name} info={char}/>
+          )
+        })
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,25 @@
 // Write your Character component here
+import styled from 'styled-components';
+
+const Outline = styled.h2`
+text-shadow: -1px -1px 0 #FFE81F, 1px -1px 0 #FFE81F, -1px 1px 0 #FFE81F, 1px 1px 0 #FFE81F;
+`
+
+const Border = styled.div`
+border-bottom: 4px solid black;
+width: 50%;
+margin-left: 25%;
+`
+
+export default function Character({info}) {
+    return (
+        <div>
+            <Outline>{info.name}</Outline>
+            <span>Birth Year: {info.birth_year}</span>
+            <ul>Films:
+                {info.films.map(film => <li key={film}>{film}</li>)}
+            </ul>
+            <Border></Border>
+        </div>
+    )
+}
